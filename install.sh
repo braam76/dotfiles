f8833447d4e92582346a8c879158a9334fa63e5c
braam76 <ilgargamidov968@gmail.com>
2025-03-17 13:58:00 +0300

    Added git configuration into install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
-# Define the source (dotfiles) and target (.config) directories
 DOTFILES_DIR="$HOME/dotfiles"
 CONFIG_DIR="$HOME/.config"
 
 # Ensure the target directory exists
 mkdir -p "$CONFIG_DIR"
 
-# Iterate over each file/directory in the dotfiles directory
+echo "[_] Creating symlinks"
+
 for item in "$DOTFILES_DIR"/*; do
-    # Extract the base name of the file/directory
     base_name=$(basename "$item")
 
-    # Define the target symlink path
     target="$CONFIG_DIR/$base_name"
 
-    # Check if a file or symlink already exists at the target location
     if [ -e "$target" ] || [ -L "$target" ]; then
         echo "Removing existing file or symlink: $target"
         rm -rf "$target"
     fi
 
-    # Create the symbolic link
     echo "Creating symlink: $target -> $item"
     ln -s "$item" "$target"
     echo ""
 done
 
-echo "Symlinks created successfully!"
+echo "[X] Symlinks created successfully!"
 
+echo "[_] Setting up Git configuration..."
+git config --global user.name "braam76"
+git config --global user.email "ilgargamidov968@gmail.com"
+git config --global init.defaultBranch main
+
+echo "[X] Git configuration applied!"
